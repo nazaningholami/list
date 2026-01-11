78a2ddd963babd55bb748444f575075dc086bab1
nazaningholami <nazaningholami670@gmail.com>
2026-01-11 08:48:01 +0430

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const addbtn = document.getElementById("addbtn");
-const input1 = document.getElementById("input").value;
-const order = document.getElementById("order").value;
+const error = document.getElementById("error")
+
 const sevebtn = document.getElementById("addtodo");
  const containerfrom = document.getElementById("container")
 const todolist = [];
+let message = "";
 addbtn.addEventListener("click",()=>{
     if(containerfrom.classList.contains("hidden")){
         containerfrom.classList.remove("hidden");
@@ -15,14 +16,32 @@ addbtn.addEventListener("click",()=>{
     }
 })
 sevebtn.addEventListener("click",(e)=>{
+    const h1 = document.createElement("h1");
     e.preventDefault();
  savetodo();
+ if(message.length>0){
+ 
+ h1.textContent = message;
+ error.appendChild(h1);
+}
 })
 function savetodo(){
+    const input1 = document.getElementById("input").value;
+const order = document.getElementById("order").value;
+if(input1 !== ""){
+
 const savetodo = {
     mytodo: input1,
     importance: order
 }
 todolist.push(savetodo);
+
 }
+else{
+    message = "لطفا یک لیستی اضافه نموده و دوباره تلاش نمایید"
+}
+}
+
+
+
 
